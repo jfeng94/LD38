5edf19ae8a4c5f1f50aca0cb474ffdc5073b42bd
Jerry Feng <jfeng94@gmail.com>
2017-04-21 22:56:23 -0700

    Added a quick and basic implementation for NPCs

---
+++ Ludum Dare 38/Assets/Scripts/Environment/NPC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+	public GameObject indicator;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Player player = other.gameObject.GetComponent<Player>();
+		if (player != null) {
+			indicator.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		Player player = other.gameObject.GetComponent<Player>();
+		if (player != null) {
+			indicator.SetActive(false);
+		}
+	}	
+}
