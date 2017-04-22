9e8a3225741e04da4ed2a4bd764c5008cc1bcb1d
Jerry Feng <jfeng94@gmail.com>
2017-04-21 21:17:58 -0700

    Camera that follows the player around

---
+++ Ludum Dare 38/Assets/Scripts/Camera/PlayerCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script allows the camera to follow the player around.
+public class PlayerCamera : MonoBehaviour {
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Make sure we're in parity with the player.
+		Vector3 position = transform.position;
+		position.x = player.transform.position.x;
+		position.y = player.transform.position.y;
+		transform.position = position;
+	}
+}
