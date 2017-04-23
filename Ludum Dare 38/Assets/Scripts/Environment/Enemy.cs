2b803c487de834282bd8f6b31dbe55121bbfac0f
timothychou <tchou@caltech.edu>
2017-04-22 22:58:49 -0700

    Changed aggro logic.

--- Ludum Dare 38/Assets/Scripts/Environment/Enemy.cs
+++ Ludum Dare 38/Assets/Scripts/Environment/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : Interactable {
 	private Player aggroTarget = null;
-
+	private Player aggroCandidate = null;
 	// Where the enemy spawns at
 	public Vector3 spawnPosition = Vector3.zero;
 
@@ -22,22 +22,38 @@ public class Enemy : Interactable {
 	void Update () {
 		// Get distance from spawn
 		float distanceFromSpawn = (transform.position - spawnPosition).magnitude;
+		
+		// Check aggro candidate.
+		if (aggroCandidate != null) {
+			float targetDistanceFromSpawn = (aggroCandidate.transform.position - spawnPosition).magnitude;
+			// Follow target if they hold aggro.
+			if (aggroTarget != null) {
 
-		// Follow the player if they are holding aggro
-		if (aggroTarget != null) {
-			// If the enemy is sufficiently far from the spawn position, lose aggro
-			if (distanceFromSpawn > leashLength) {
-				aggroTarget = null;
+				// If the target is sufficiently far from the spawn position, lose aggro
+				if (targetDistanceFromSpawn > leashLength) {
+					
+					base.OnPlayerCannotInteract (aggroTarget);
+					aggroTarget = null;
+				}
+
+				// Otherwise, move towards the player
+				else {
+					MoveTowardsPosition (aggroTarget.transform.position);
+				}
 			}
-
-			// Otherwise, move towards the player
-			else {
-				MoveTowardsPosition(aggroTarget.transform.position);
+			// Check if aggro candidate is back in leash range.
+			else if (targetDistanceFromSpawn <= leashLength) {
+				base.OnPlayerCanInteract (aggroCandidate);
+				aggroTarget = aggroCandidate;
+				
+			} else {
+				
+				MoveTowardsPosition(spawnPosition);
 			}
 		}
 
-		// If there is no aggro, move towards the spawn point
-		if (aggroTarget == null) {
+		// If there is no aggro candidate, return to spawn point.
+		if (aggroCandidate == null) {
 			MoveTowardsPosition(spawnPosition);
 		}
 	}
@@ -47,9 +63,16 @@ public class Enemy : Interactable {
 
 		if (player != null) {
 			aggroTarget = player;
+			aggroCandidate = player;
 		}
 	}
 
+	public override void OnPlayerCannotInteract(Player player) {
+		base.OnPlayerCannotInteract (player);
+
+		aggroTarget = null;
+		aggroCandidate = null;
+	}
 
 	protected virtual void MoveTowardsPosition(Vector3 position) {
 		Vector3 displacement = position - transform.position;
@@ -57,6 +80,8 @@ public class Enemy : Interactable {
 		// Get rid of any vertical component
 		displacement.y = 0;
 
+		// Always push left?
+		displacement.x = -1;
 		// Turn displacement into a direction
 		Vector3 direction = displacement.normalized;
 
